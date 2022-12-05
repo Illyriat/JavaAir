dfd4f9c14a81e126c8b7c8bb0124adda3cb7c417
Illyriat <109182598+Illyriat@users.noreply.github.com>
2022-12-05 03:01:41 +0000

    Plane Test passing

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+import plane.Plane;
+import plane.PlaneType;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+    Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.BOEING737);
+    }
+
+    @Test
+    public void planeHasType() {
+        assertEquals(PlaneType.BOEING737, plane.getPlaneType());
+    }
+
+    @Test
+    public void planeHasCapacity() {
+        assertEquals(260, plane.getPlaneType().getCapacity());
+    }
+
+    @Test
+    public void planeHasMaxWeight() {
+        assertEquals(675000.50, plane.getPlaneType().getWeight(), 0.0);
+    }
+
 }
